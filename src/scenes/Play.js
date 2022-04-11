21ae601e5ed34a57a440c112bd36bff6b94a89e8
Michael Phannavong <mphannav@ucsc.edu>
2022-04-11 03:59:57 -0700

    started collision

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -23,9 +23,38 @@ class Play extends Phaser.Scene {
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
+        //spaceships
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
     }
     update(){
-        this.starfield.tilePositionX -= 2;
+        this.starfield.tilePositionX -= 4;
         this.p1Rocket.update();
+        this.ship01.update();               
+        this.ship02.update();
+        this.ship03.update();
+
+        if(this.checkCollision(this.p1Rocket, this.ship03)) {
+            console.log('kaboom ship 03');
+          }
+          if (this.checkCollision(this.p1Rocket, this.ship02)) {
+            console.log('kaboom ship 02');
+          }
+          if (this.checkCollision(this.p1Rocket, this.ship01)) {
+            console.log('kaboom ship 01');
+          }
+    }
+    checkCollision(rocket, ship){
+        if (rocket.x < ship.x + ship.width && 
+            rocket.x + rocket.width > ship.x && 
+            rocket.y < ship.y + ship.height &&
+            rocket.height + rocket.y > ship. y) {
+                return true;
+        } 
+        else {
+            return false;
+        }
     }
 }
