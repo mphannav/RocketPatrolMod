31ee2d626fa39d80ec9484511d09675bb2c6da37
Michael Phannavong <mphannav@ucsc.edu>
2022-04-21 03:41:15 -0700

    explosion mod

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -9,7 +9,7 @@ class Play extends Phaser.Scene {
         this.load.image('spaceship2', './assets/spaceship.png');
         this.load.image('starfield', './assets/starsy.png');
         //this.load.image('starfield', './assets/moon.png');
-        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 32, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
 
     create(){
