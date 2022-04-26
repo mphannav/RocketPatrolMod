2d7bf821f826a191420086da623fc83784b844a4
Michael Phannavong <mphannav@ucsc.edu>
2022-04-26 04:15:44 -0700

    new comments

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+//Michael Phannavong
+//Rocket Patrol MOD
+//Took about 25+ hours
 let config = {
     type: Phaser.CANVAS,
     width: 640,
@@ -9,3 +12,13 @@ let keyW, keyR, keyLEFT, keyRIGHT, keyA, keyD, keyUP;
 
 let borderUISize = config.height / 15;
 let borderPadding = borderUISize / 3;
+
+// (30) simultaneous two-player mode 
+// (20) created new artwork for all in game assets 
+// (20) created a new spaceship worth more points and is faster 
+// (10) created a new title screen 
+// (5-10) replace the UI borders with new artwork
+// (5) added own copyright-free bg music 
+// (5) created new scrolling tile sprite for background
+// (5) playing can control rocket after Firing 
+// (5 self assessment) created a separate p2 score
